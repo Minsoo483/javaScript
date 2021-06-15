30e1c48300f65cbc37386bd07afdb3b524272e8a
lhw2106@naver.com <lhw2106@naver.com>
2021-06-15 13:11:50 +0900

    js추가파일

--- js/event.js
+++ js/event.js
@@ -1,8 +1,14 @@
 function showText(){
     var text = document.getElementById("desc");
     text.style.display = "block";
+
+    var button = document.getElementById("open");
+    button.style.display = "none";
 }
 function hideText(){
     var text = document.getElementById("desc");
     text.style.display = "none";
+
+    var button = document.getElementById("open");
+    button.style.display = "block";
 }
